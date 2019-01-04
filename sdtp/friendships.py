1703d9c4232af3969405819f704b8356cd7980a7
Renato Callado Borges <rborges@hush.com>
2019-01-04 11:05:25 -0200

    Refactored check_for_commands in friendship. Added print who is friends with player.

--- sdtp/friendships.py
+++ sdtp/friendships.py
@@ -49,104 +49,67 @@ class Friendships(threading.Thread):
 
     def check_for_command(self, match_groups):
         self.logger.debug("check_for_command({})".format(match_groups))
-        matcher = re.compile("^/friend[\s]*(.*)$")
+        matcher = re.compile("^/(friend)[\s]*(.*)$")
         matches = matcher.search(match_groups[11])
         if not matches:
             self.logger.debug("No command detected.")
             return
-        argument = matches.groups()[0].strip()
-        self.logger.debug("Argument detected: '{}'".format(argument))
-        command = ""
-        name = argument
-        if len(argument) > 0:
-            if argument[-1] in ["-", "+"]:
-                command = argument[-1]
-                name = argument[:-1]
-            if "alias " in argument:
-                command = "alias"
-                name = argument[6:].strip()
-        self.logger.info("argument = {}, command = {}, name = {}".format(
-            argument, command, name))
-        db_answer = self.controller.database.blocking_consult(
-            lkp_table,
-            [(lkp_table.steamid, "==", match_groups[7])])
-        if len(db_answer) > 1:
-            self.logger.info("DB entry non unique: {}".format(db_answer))
+        command = matches.groups()[0].strip()
+        arguments = matches.groups()[1].strip().split(" ")
+        self.logger.info("command: '{}', arguments: {}".format(
+            command, arguments))
+        
+        name = ""
+        subcommand = "print"
+        if len(arguments) == 1:
+            if arguments[0] != "":
+                name = arguments[0]
+                subcommand = "toggle"
+                if name[-1] == "-":
+                    subcommand = "delete"
+                    name = name[:-1]
+                if name[-1] == "+":
+                    subcommand = "add"
+                    name = name[:-1]
+        if len(arguments) == 2:
+            if arguments[0] == "alias":
+                name = arguments[1].strip()
+                subcommand = "alias"
+        self.logger.info("subcommand = {}, name = {}".format(subcommand, name))
+
+        player = self.controller.worldstate.get_player_steamid(match_groups[7])
+        other = self.controller.worldstate.get_player_string(name)
+        
+        if command == "friend":
+            self.command_friend(subcommand, player, other)
             return
-        player = db_answer[0]
-        if command == "":
-            if argument == "":
-                self.list_player_friends(player)
-                return
-        if command == "alias":
+
+    def command_friend(self, subcommand, player, other):
+        if subcommand == "add":
+            self.add_friendship(player, other)
+            return
+        if subcommand == "alias":
             self.create_alias(player, name)
             return
-        self.toggle_friendship(player, name, argument)
-            
-    def toggle_friendship(self, player, name, argument):
-        other = self.controller.worldstate.get_player_string(name)
-        if other is None:
-            self.controller.server.pm(
-                player, "No player named \'{}\' found.".format(name))
+        if subcommand == "delete":
+            self.delete_friendship(player, other)
             return
-        friend = other
-        db_answer = self.controller.database.blocking_consult(
-            FriendshipsTable,
-            [(FriendshipsTable.player_steamid, "==", player["steamid"]),
-             (FriendshipsTable.friend_steamid, "==", friend["steamid"])])
-        existing_friendship = False
-        if len(db_answer) == 1:
-            existing_friendship = True
-        if existing_friendship and argument[-1] == "+":
-            self.controller.server.pm(
-                player, "You are already friends with {}.".format(
-                    friend["name"]))
+        if subcommand == "print":
+            self.print_friendships(player)
             return
-        if existing_friendship and (argument[-1] == "-" or \
-                                    friend["name"] == argument):
-            self.controller.database.blocking_delete(
-                FriendshipsTable,
-                [(FriendshipsTable.player_steamid, "==", player["steamid"]),
-                 (FriendshipsTable.friend_steamid, "==", friend["steamid"])])
-            self.controller.server.pm(
-                player, "Removed friendship with {}.".format(friend["name"]))
+        if subcommand == "toggle":
+            self.toggle_friendship(player, other)
             return
-        if argument[-1] == "-":
-            self.controller.server.pm(
-                player, "You cannot delete friendships you don\'t have.")
-            return
+
+    def add_friendship(self, player, other):
         self.controller.database.blocking_add(
             FriendshipsTable,
             [FriendshipsTable(
                 player_steamid = player["steamid"],
-                friend_steamid = friend["steamid"])])
+                friend_steamid = other["steamid"])])
         self.controller.server.pm(
-            player, "You are now friends with {}.".format(friend["name"]))
-
-    def list_player_friends(self, player):
-        db_answer = self.controller.database.blocking_consult(
-            FriendshipsTable,
-            [(FriendshipsTable.player_steamid, "==", player["steamid"])])
-        for friendship in db_answer:
-            friend = self.controller.database.blocking_consult(
-                lkp_table,
-                [(lkp_table.steamid, "==", friendship["friend_steamid"])])[0]
-            try:
-                response += ", {}".format(friend["name"])
-            except:
-                response = "{}".format(friend["name"])
-        try:
-            self.controller.server.pm(player, "Your friends are: {}.".format(
-                response))
-        except UnboundLocalError:            
-            self.controller.server.pm(
-                player, "You do not have any friends (yet)!")
-        db = self.controller.database.blocking_consult(
-            AliasTable, [(AliasTable.steamid, "==", player["steamid"])])
-        if len(db) == 1:
-            self.controller.server.pm(player, "You have an alias: {}.".format(
-                db[0]["alias"]))
-
+            player, "You are now friends with {}.".format(other["name"]))
+        
     def create_alias(self, player, name):
         self.logger.info("{} setting alias {}.".format(player["name"], name))
         other = self.controller.worldstate.get_player_string(name)
@@ -173,5 +136,78 @@ class Friendships(threading.Thread):
             return
         self.logger.error("Multiple DB entries in AliasTable for {}.".format(
             player["name"]))
+        
+    def delete_friendship(self, player, other):
+        self.controller.database.blocking_delete(
+            FriendshipsTable,
+            [(FriendshipsTable.player_steamid, "==", player["steamid"]),
+             (FriendshipsTable.friend_steamid, "==", other["steamid"])])
+        self.controller.server.pm(
+            player, "{} is no longer your friend.".format(other["name"]))
+        
+    def print_friendships(self, player):
+        db_answer = self.controller.database.blocking_consult(
+            FriendshipsTable,
+            [(FriendshipsTable.player_steamid, "==", player["steamid"])])
+        for friendship in db_answer:
+            friend = self.controller.database.blocking_consult(
+                lkp_table,
+                [(lkp_table.steamid, "==", friendship["friend_steamid"])])[0]
+            try:
+                response += ", {}".format(friend["name"])
+            except:
+                response = "{}".format(friend["name"])
+        try:
+            self.controller.server.pm(player, "Your friends are: {}.".format(
+                response))
+        except UnboundLocalError:            
+            self.controller.server.pm(
+                player, "You do not have any friends (yet)!")
+
+        db = self.controller.database.blocking_consult(
+            FriendshipsTable,
+            [(FriendshipsTable.friend_steamid, "==", player["steamid"])])
+        self.logger.info("db = {}".format(db))
+        response = None
+        for friendship in db:
+            friend = self.controller.database.blocking_consult(
+                lkp_table,
+                [(lkp_table.steamid, "==", friendship["player_steamid"])])[0]
+            try:
+                response += ", {}".format(friend["name"])
+            except:
+                response = "{}".format(friend["name"])
+        try:
+            self.controller.server.pm(
+                player, "People that have you as friend are: {}.".format(
+                    response))
+        except UnboundLocalError:            
+            self.controller.server.pm(
+                player, "Nobody has you as friend (yet)!")
+            
+        db = self.controller.database.blocking_consult(
+            AliasTable, [(AliasTable.steamid, "==", player["steamid"])])
+        if len(db) == 1:
+            self.controller.server.pm(player, "You have an alias: {}.".format(
+                db[0]["alias"]))
+
+    def toggle_friendship(self, player, other):
+        if other is None:
+            self.controller.server.pm(
+                player, "No player named \'{}\' found.".format(name))
+            return
+        friend = other
+        db_answer = self.controller.database.blocking_consult(
+            FriendshipsTable,
+            [(FriendshipsTable.player_steamid, "==", player["steamid"]),
+             (FriendshipsTable.friend_steamid, "==", friend["steamid"])])
+        existing_friendship = False
+        if len(db_answer) == 1:
+            existing_friendship = True
+        if existing_friendship:
+            self.delete_friendship(player, other)
+            return
+        self.add_friendship(player, other)
+
 
                                                   
